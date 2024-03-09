d0bd464e4389918d89b8c9863c1aef8dd90124c1
ferradermis-user <83044229+ferradermis-user@users.noreply.github.com>
2024-03-08 20:41:32 -0600

    PathPlanner path tuning :))))

--- src/main/java/frc/robot/subsystems/DriveSubsystem.java
+++ src/main/java/frc/robot/subsystems/DriveSubsystem.java
@@ -98,7 +98,7 @@ public class DriveSubsystem extends SubsystemBase {
                 this::getRobotRelativeSpeeds, // ChassisSpeeds supplier. MUST BE ROBOT RELATIVE
                 this::driveRobotRelative, // Method that will drive the robot given ROBOT RELATIVE ChassisSpeeds
                 new HolonomicPathFollowerConfig( // HolonomicPathFollowerConfig, this should likely live in your Constants class
-                        new PIDConstants(2, 0, 0), // Translation PID constants
+                        new PIDConstants(2.5, 0, 0), // Translation PID constants
                         new PIDConstants(5, 0, 0), // Rotation PID constants
                         4.5, // Max module speed, in m/s
                         0.324, // Drive base radius in meters. Distance from robot center to furthest module.
