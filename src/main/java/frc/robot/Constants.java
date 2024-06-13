34da0542411fedbf629f6c7f93d3a99da14fe0fd
ferradermis-user <ferradermis@gmail.com>
2024-06-13 14:39:36 -0500

    messed with max speed for demo, is back to normal

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -28,7 +28,7 @@ public final class Constants {
   public static final class DriveConstants {
     // Driving Parameters - Note that these are not the maximum capable speeds of
     // the robot, rather the allowed maximum speeds
-    public static final double kMaxSpeedMetersPerSecond = 5.74; //CHANGE WHEN TESTING LIMELIGHT FOR EVERYONES SAKE MonkaS
+    public static final double kMaxSpeedMetersPerSecond = 5.74; //normally 5.74 {CHANGE WHEN TESTING LIMELIGHT FOR EVERYONES SAKE MonkaS}
     public static final double kMaxAngularSpeed = 2 * Math.PI; // radians per second
 
     public static final double kDirectionSlewRate = 1.2; // radians per second
